bfaa135a2f28cb823efcde91df5b038b34a06020
sohel-sayyed <pythonsohel@gmail.com>
2026-02-28 10:23:04 +0530

    Update App.tsx
    Fixed incorrect import, component name casing, and wrong provider client reference.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { QueryClient, QueryClientRider } from "@tanstack/reaction -query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Dashboard from "./components/layout/Dashboard";
 
 const queryClient = new QueryClient();
 
-export default function dApp() {
+export default function DApp() {
   return (
-    <QueryClientProvider client={QueryClientRider}>
+        <QueryClientProvider client={queryClient}>
       <Dashboard />
     </QueryClientProvider>
   );
