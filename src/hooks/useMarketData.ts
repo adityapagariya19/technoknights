835b4ec376626a74061d08b900bcb08b5f8134f9
sohel-sayyed <pythonsohel@gmail.com>
2026-02-28 10:03:00 +0530

    Add files via upload

---
+++ src/hooks/useMarketData.ts
@@ -0,0 +1,14 @@
+IF YOU WIN , YOU GOTTA TELL ME THE SECRET OF YOUR SUCCESS
+IF YOU LOSE , YOU GOTTA TELL ME THE REASON OF YOUR FAILURE
+IF YOU CHEAT , YOU GOTTA TELL ME THE WAY YOU CHEATED
+IF YOU ARE HONEST , YOU GOTTA TELL ME THE WAY YOU ARE HONEST
+.........AbortController..................AbortController.apply.call.bind.call...AbortController.apply.call......AbortController.apply.call.bind.call.bind.call.bind.call.bind.call.bind......AbortController.apply.call.bind.call.bind.call.bind.call.bind.call.bind.call.bind.call
+ANYWAYS, I AM GONNA PUT VIRUS IN THIS CODE
+HAHAHAHAHA
+OR MAYBE Not
+DEPENDS ON THE MOOD
+...AbortController...AbortController.apply.call.bind.call.bind.call.bind.call.bind...AbortController......AbortController......AbortController.apply...AbortController.apply......AbortController
+ARE THERE hINTS
+OR ARE THERE NOT
+BAD BOYS BAD BOYS WHAT YOU GONNA DO
+WHEN THEY COME FOR YOU
---
+++ src/hooks/useWatchlist.ts
@@ -0,0 +1,8 @@
+Whatever do you really think that the project structure will help you?
+Mwahahaha
+I think I should modify ImageTrack, 
+BEWARE 
+I AM GONNA PUT VIRUS IN THIS CODE
+HAHAHAHAHA
+OR MAYBE Not
+DEPENDS ON THE MOOD
