5f0cc3d9cab80ceb22f6745ef362d6db02cbb608
sohel-sayyed <pythonsohel@gmail.com>
2026-02-28 10:43:59 +0530

    Update Card.tsx
    Added proper TypeScript typings for function and React component props.

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-function cn(...inputs: any[]) {
+export function cn(...inputs: any[]) {
  return twMerge(clsx(...inputs));
 }
 
-export const Card = ({ className, children }) => (
+export const Card: React.FC<{ className?: string; children: React.ReactNode }> = ({ className, children }) => (
   <div
     className={cn(
       "bg-[#151619] border border-[#141414] rounded-xl overflow-hidden",
